fb2550ee785364055a231279cd8d7bbe055bd27b
Thomason Zhou <thomason.zhou@uwaterloo.ca>
2024-12-16 21:09:00 -0700

    Add linear layers and forward pass to enable training with tensors

--- project/run_tensor.py
+++ project/run_tensor.py
@@ -21,8 +21,10 @@ class Network(minitorch.Module):
         self.layer3 = Linear(hidden_layers, 1)
 
     def forward(self, x):
-        # TODO: Implement for Task 2.5.
-        raise NotImplementedError("Need to implement for Task 2.5")
+        x = self.layer1.forward(x).relu()
+        x = self.layer2.forward(x).relu()
+        x = self.layer3.forward(x).sigmoid()
+        return x
 
 
 class Linear(minitorch.Module):
@@ -33,8 +35,10 @@ class Linear(minitorch.Module):
         self.out_size = out_size
 
     def forward(self, x):
-        # TODO: Implement for Task 2.5.
-        raise NotImplementedError("Need to implement for Task 2.5")
+        batch_size, _ = x.shape
+        return (x.view(*x.shape, 1) * self.weights.value.view(1, *self.weights.value.shape)).sum(
+            1
+        ).view(batch_size, self.out_size) + self.bias.value.view(1, self.out_size)
 
 
 def default_log_fn(epoch, total_loss, correct, losses):
@@ -53,7 +57,6 @@ class TensorTrain:
         return self.model.forward(minitorch.tensor(X))
 
     def train(self, data, learning_rate, max_epochs=500, log_fn=default_log_fn):
-
         self.learning_rate = learning_rate
         self.max_epochs = max_epochs
         self.model = Network(self.hidden_layers)
