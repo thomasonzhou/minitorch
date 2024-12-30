4a08460ac0aacc7248fc782f9b92cd859c9407ba
Thomason Zhou <thomason.zhou@uwaterloo.ca>
2024-12-29 21:44:46 -0700

    Expose minitorch.autograd.Function to enable extension

--- minitorch/autograd/__init__.py
+++ minitorch/autograd/__init__.py
@@ -1,3 +1,4 @@
 """Defines the base functional class for forward and backward passes"""
 
 from .autodiff import Variable, Context, backpropagate, central_difference  # noqa: F401
+from minitorch._tensor_functions import Function
